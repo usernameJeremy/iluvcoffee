602349f8ea8b1ec4877f9c74c032fedf5baa570a
usernameJeremy <Jeremy_koster@outlook.com>
2023-05-02 22:07:11 +0200

    cleared Chapter 1 added services and modules

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,32 +1,47 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
+
+    constructor(
+        private readonly coffeesService: CoffeesService,
+    ){}
     
-    @Get('flavors')
-    findAll() {
-        return 'This action returns all coffees';
+    @Get()
+    findAll(@Query() paginationQuerry) {
+       // const {limit, offset} = paginationQuerry;
+       return this.coffeesService.findAll();
+        
     }
 
     @Get(':id')
     findOne(@Param('id') id: string){
-        return `This action returns #${id} coffee`;
+        return  this.coffeesService.findOne(id);
+        
     }
 
 @Post()
-create(@Body() body){
-    return body;
-    //return 'This action creates a coffee'
+create(@Body() createCoffeeDto: CreateCoffeeDto){
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeesService.create(createCoffeeDto);
+    
+    
 }
 
 @Patch(':id')
-update(@Param('id')id: string , @Body() body){
-    return `This action updates #${id} coffee`;
+update(@Param('id')id: string , @Body() updateCoffeeDto : UpdateCoffeeDto){
+
+    return this.coffeesService.update(id, updateCoffeeDto);
+    
 }
 
 @Delete(':id')
 remove(@Param('id') id: string){
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
+    
 }
 
 }
